dfcf38915e1c71c6e3d2b8b8580bb03ac49a2635
Aryan Dewangan <devcorp25@gmail.com>
2026-01-29 21:24:41 -0800

    Revert Something

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ function sayname() {
     console.log('My name is ' + name1)
 }
 
+<<<<<<< HEAD
 function newFun() {
     console.log("Hey I'm Here")
+}=======
+function isAbove18() {
+    return age >= 18
+>>>>>>> parent of 4fd433e (Remove 18 function)
 }
